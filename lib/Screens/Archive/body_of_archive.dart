de8b2f9f6fa4c24a93b0b2e9ba15b99173a038f6
hadeer10 <61805398+hadeer10@users.noreply.github.com>
2021-04-29 19:35:38 +0200

    Update body_of_archive.dart

--- lib/Screens/Archive/body_of_archive.dart
+++ lib/Screens/Archive/body_of_archive.dart
@@ -1,10 +1,12 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Login/components/background.dart';
 import 'package:flutter_auth/Screens/profile/profile.dart';
 import 'package:flutter_auth/constants.dart';
+import '../../init.dart';
+
 
 class ArchiveBody extends StatefulWidget {
+  static String id = 'ArchiveBody';
   @override
   _ArchiveBodyState createState() => _ArchiveBodyState();
 }
@@ -15,143 +17,162 @@ class _ArchiveBodyState extends State<ArchiveBody> {
   var grey = Colors.grey;
   bool txt = true;
   bool isLiked = true;
-
   @override
   Widget build(BuildContext context) {
     return Background(
       child: Padding(
         padding: const EdgeInsets.all(1.0),
         child: ListView.builder(
-          itemCount: 1,
-          itemBuilder:(context , i) => /*Dismissible(
-            key: Key(),
+          itemCount: archive.length,
+          itemBuilder: (context, i) =>
+              Dismissible(
+            key: Key(archive[i].name),
             background: Container(
-              alignment: Alignment.centerLeft,
+              alignment: Alignment.centerRight,
               color: kPrimaryLightColor,
               child: Icon(
-                Icons.archive,
+                Icons.delete,
                 color: kPrimaryColor,
               ),
             ),
             onDismissed: (direction) {
               setState(() {
-
+                archive.removeAt(i);
               });
             },
-      */
-          Card(
-        shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      child: Column(
-        children: [
-            SizedBox(
-              height: 5,
-            ),
-            Column(children: [
-              Row(
+            direction: DismissDirection.endToStart,
+            child:
+
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Column(
                 children: [
-                  Row(
-                    children: [
-                      FlatButton(
-                        child: image == null ? Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                border: Border.all(width: 2, color: kPrimaryLightColor)),
-                            child: ClipOval(child: Icon(Icons.camera_alt_outlined,  size: 50.0 ,))):
-                        Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(100),
-                              border: Border.all(width: 2, color: kPrimaryLightColor)),
-                          child: ClipOval(
-                            child: Image.file(
-                              image,
-                              height: 50,
-                              width: 50,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => profile(),
-                            ),
-                          );
-                        },
-                      ),
-                      TextButton(
-                        child: Column(
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Column(children: [
+                    Row(
+                      children: [
+                        Row(
                           children: [
-                            Text( "Hadeer Hassan" , style: TextStyle(color: Colors.black),
+                            FlatButton(
+                              child: image == null
+                                  ? Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(100),
+                                          border: Border.all(
+                                              width: 2,
+                                              color: kPrimaryLightColor)),
+                                      child: ClipOval(
+                                          child: Icon(
+                                        Icons.camera_alt_outlined,
+                                        size: 50.0,
+                                      )))
+                                  : Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(100),
+                                          border: Border.all(
+                                              width: 2,
+                                              color: kPrimaryLightColor)),
+                                      child: ClipOval(
+                                        child: Image.file(
+                                          image,
+                                          height: 50,
+                                          width: 50,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => profile(),
+                                  ),
+                                );
+                              },
                             ),
-                            Text( "Hadeer@gmail.com" , style: TextStyle(color: Colors.grey),),
+                            TextButton(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Hadeer Hassan",
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                  Text(
+                                    "Hadeer@gmail.com",
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => profile(),
+                                  ),
+                                );
+                              },
+                            ),
                           ],
                         ),
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>profile(),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              ListTile(
-                title: Text('mm'),
-                subtitle: Text('jj'),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(
-                          icon: Icon(
-                            Icons.favorite,
-                            color: isLiked ? grey : red,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              isLiked = !isLiked;
-                              txt = !txt;
-                            });
-                          },
-                        ),
-                        SizedBox(
-                          width: 6,
-                        ),
-                        Text(txt ? '0' : '1'),
                       ],
                     ),
-                    Row(
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.message),
-                          onPressed: () {},
-                        ),
-                        SizedBox(
-                          width: 3,
-                        ),
-                        Text('message'),
-                      ],
-                    )
-                  ],
-                ),
+                    ListTile(
+                      title: Text(archive[i].name),
+                      subtitle: Text(archive[i].status),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: Icon(
+                                  Icons.favorite,
+                                  color: isLiked ? grey : red,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    isLiked = !isLiked;
+                                    txt = !txt;
+                                  });
+                                },
+                              ),
+                              SizedBox(
+                                width: 6,
+                              ),
+                              Text(txt ? '0' : '1'),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.message),
+                                onPressed: () {},
+                              ),
+                              SizedBox(
+                                width: 3,
+                              ),
+                              Text('message'),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ]),
+                ],
               ),
-            ]),
-        ],
-      ),
-    ),
+            ),
           ),
         ),
+      ),
     );
   }
 }
