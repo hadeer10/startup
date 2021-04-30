77ab5df9b8d47102f9c77a75fcda3a286887ca4c
mohanadNaf <mido.nafaa@yahoo.com>
2021-04-30 02:29:31 +0200

    add init.dart

--- lib/init.dart
+++ lib/init.dart
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import 'models/startup.dart';
 
 List<StartUp> archive = [];
@@ -18,31 +18,7 @@ List<StartUp> startupsList = [
       about: 'company for Software ',
       status: 'Successed'),
   StartUp(
-      name: 'sixth Startup',
+      name: 'fifth Startup',
       about: 'company for Software ',
       status: 'Successed'),
-=======
-import 'models/startup.dart';
-
-List<StartUp> archive = [];
-List<StartUp> startupsList = [
-  StartUp(
-      name: 'First Startup',
-      about: 'company for Software ',
-      status: 'Successed'),
-  StartUp(
-      name: 'Second  Startup',
-      about: 'company for Furniture ',
-      status: 'Failed'),
-  StartUp(
-      name: 'Third Startup', about: 'company for Cars ', status: 'Successed'),
-  StartUp(
-      name: 'fourth Startup',
-      about: 'company for Software ',
-      status: 'Successed'),
-  StartUp(
-      name: 'sixth Startup',
-      about: 'company for Software ',
-      status: 'Successed'),
->>>>>>> origin/main
 ];
